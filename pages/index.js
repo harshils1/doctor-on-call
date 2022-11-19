119e0cfa1c32a337506317cbc413967b03bc6f49
Soham Parmar <69543944+SohamP2812@users.noreply.github.com>
2022-11-19 14:53:22 -0700

    header (#6)
    Co-authored-by: Soham Parmar <soham.parmar@faire.com>

--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,7 @@ import styles from '../styles/Home.module.css'
 import { useEffect } from 'react'
 import { db } from '../firebase-client/config'
 import { setDoc, doc } from 'firebase/firestore'
+import Header from '../components/Header'
 
 export default function Home() {
   return (
@@ -13,6 +14,7 @@ export default function Home() {
         <meta name="description" content="Doctor On Call" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <Header />
     </div>
   )
 }
