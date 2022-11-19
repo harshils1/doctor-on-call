e5cccfd7efd4ff66b251b137ad389e8add899b78
Soham Parmar <soham.parmar@faire.com>
2022-11-19 10:48:38 -0700

    env

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,20 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import { useEffect } from 'react'
+import { db } from '../firebase-client/config'
+import { setDoc, doc } from 'firebase/firestore'
 
 export default function Home() {
+  useEffect(() => {
+    ;(async () => {
+      await setDoc(doc(db, 'cities', 'LA'), {
+        name: 'Los Angeles',
+        state: 'CA',
+        country: 'USA',
+      })
+    })()
+  }, [])
   return (
     <div className={styles.container}>
       <Head>
