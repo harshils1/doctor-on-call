abac2e15803696599e1db7e9c47b988f64c2b3e2
Mansehaj Singh <76667723+mansehajsingh@users.noreply.github.com>
2022-11-19 19:14:23 -0500

    Request creation page. (#8)

---
+++ components/DoctorRow.js
@@ -0,0 +1,19 @@
+import styles from "../styles/DoctorRow.module.css";
+import { FaUserAlt } from "react-icons/fa";
+
+export default function DoctorRow({ details, selected = false, onClick }) {
+    return (
+        <div 
+            className={styles.container}
+            style={{ border: `solid ${ selected ? "2px #4eaa7b" : " 1px #e6e6e6" }` }}
+            onClick={() => onClick(details)}
+        >
+            <FaUserAlt size={40} color="#d4d4d4" className={styles.profile_pic}/>
+            <div className={styles.details}>
+                <p className={styles.name}>{details?.firstName} {details?.lastName}</p>
+                <p className={styles.specialty}><b>Specialty:</b> {details?.specialty}</p>
+                <p className={styles.rating}><b>Rating:</b> </p>
+            </div>
+        </div>
+    )
+}
---
+++ pages/create-request.js
@@ -0,0 +1,208 @@
+import {
+    Flex,
+    Box,
+    FormControl,
+    FormLabel,
+    Input,
+    Checkbox,
+    Stack,
+    Link,
+    Button,
+    Heading,
+    Text,
+    useColorModeValue,
+    Textarea,
+    CheckboxGroup,
+    useToast,
+  } from '@chakra-ui/react'
+  import NextLink from 'next/link'
+  import { useEffect, useState } from 'react';
+  import { useRouter } from 'next/router';
+  import { auth, db } from '../firebase-client/config';
+import {
+  onAuthStateChanged,
+} from 'firebase/auth'
+import { getDoc, doc, getDocs, query, collection, where, addDoc } from 'firebase/firestore';
+import DoctorRow from '../components/DoctorRow';
+
+export default function CreateRequestPage () {
+
+    const router = useRouter();
+
+    const [user, setUser] = useState(null);
+
+    const [doctors, setDoctors] = useState([]);
+
+    const [selectedDoctors, setSelectedDoctors] = useState([]);
+
+    const [formState, setFormState] = useState({
+        title: "",
+        message: "",
+    });
+
+    const toast = useToast();
+
+    const [created, setCreated] = useState(false);
+
+    useEffect(() => {
+        const unsubscribe = onAuthStateChanged(auth, async (user) => {
+          if (!user) {
+            router.push('/signup');
+          } else {
+            const userDoc = await getDoc(doc(db, 'users', user.uid));
+            setUser(userDoc.data());
+          }
+        })
+    
+        return () => unsubscribe()
+      }, [])
+
+    useEffect(() => {
+
+        if (user) {
+            const equals = "==";
+
+            const fetchDoctorsInArea = async () => {
+        
+                const q = query(
+                    collection(db, 'users'), 
+                    where("city", equals, user.city),
+                    where("userType", equals, "Doctor")
+                );
+        
+                const doctorsSnapshot = await getDocs(q);
+                setDoctors(doctorsSnapshot.docs.map((doc) => doc.data()));
+            }
+
+            fetchDoctorsInArea();
+        }
+
+    }, [user]);
+
+    useEffect(() => {
+
+        if (created) {
+            toast({
+                title: 'Request created successfully.',
+                status: 'success',
+                duration: 3000,
+                isClosable: true,
+            });
+            router.push("/");
+        }
+    }, [created]);
+
+    const handleFormChange = (e) => {
+        setFormState({
+            ...formState,
+            [e.target.name]: e.target.value
+        });
+    }
+
+    const setSelected = (details) => {
+        let isSelected = false;
+        selectedDoctors.forEach((doctor) => {
+            if (doctor.uid === details.uid) {
+                isSelected = true;
+            }
+        });
+
+        if (isSelected) {
+            setSelectedDoctors(selectedDoctors.filter((doctor) => {
+                return doctor.uid !== details.uid;
+            }))
+        } else {
+            setSelectedDoctors([...selectedDoctors, details]);
+        }
+    }
+
+    const isSelected = (uid) => {
+        let selected = false;
+
+        selectedDoctors.forEach(doctor => {
+            if (doctor.uid === uid) {
+                selected = true;
+            }
+        })
+
+        return selected;
+    }
+
+    const createRequest = async () => {
+        await addDoc(collection(db, "requests"), {
+            title: formState.title,
+            message: formState.message,
+            requestingUser: user,
+            receivingDoctors: selectedDoctors,
+            acceptingDoctorUid: null,
+            acceptingDoctorResponse: null,
+            fulfilled: false
+        });
+
+        setCreated(true); 
+    }
+
+    return (
+        <Flex
+            minH={'100vh'}
+            align={'center'}
+            justify={'center'}
+            justifyContent={'center'}
+            bg={useColorModeValue('gray.50', 'gray.800')}
+        >
+        <Stack spacing={8} mx={'auto'} maxW={'lg'} py={12} px={6}>
+            <Stack align={'center'}>
+            <Heading fontSize={'4xl'}>Request a Doctor</Heading>
+            </Stack>
+            <Box
+                rounded={'lg'}
+                bg={useColorModeValue('white', 'gray.700')}
+                boxShadow={'lg'}
+                p={8}
+                width="500px"
+            >
+            <Stack spacing={4}>
+                <FormControl id="title">
+                    <FormLabel>Title</FormLabel>
+                    <Input name="title" onChange={handleFormChange} />
+                </FormControl>
+                <FormControl id="message">
+                <FormLabel>Message (Include symptoms, relevant medical history, any important details)</FormLabel>
+                <Textarea rows={10} name="message" onChange={handleFormChange}/>
+                </FormControl>
+
+                <Stack>
+                    <FormControl id="doctors">
+                    <FormLabel>Doctors available in {user?.city}, {user?.state}, {user?.country}</FormLabel>
+                    <Stack height={"300px"} overflowY={"auto    "}>
+                        { doctors?.map((doctor) => {
+                            return (
+                                <DoctorRow 
+                                    details={doctor} 
+                                    selected={isSelected(doctor.uid)}
+                                    onClick={setSelected}
+                                />
+                            );
+                        }) }
+                    </Stack>
+                    </FormControl>
+                </Stack>
+
+                <Stack spacing={10}>
+                <Button
+                    bg={'blue.400'}
+                    color={'white'}
+                    _hover={{
+                    bg: 'blue.500',
+                    }}
+                    onClick={createRequest}
+                >
+                    Create
+                </Button>
+                </Stack>
+            </Stack>
+            </Box>
+        </Stack>
+        </Flex>
+    );
+}
